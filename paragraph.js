2d528e52a7e7581b1e3d7f9d14cef0ceb76a1819
NETHDIMA <154481796+nethdima@users.noreply.github.com>
2024-02-18 16:25:18 +0530

    Add files via upload

---
+++ paragraph.js
@@ -0,0 +1,6 @@
+var paragraph = document.getElementById("support-us");
+var text = document.createTextNode(
+  "Join our broadcast and media production company in our mission to bring quality content to our audience. Your support is crucial for us to continue creating engaging and entertaining broadcasts. To contribute, please donate to our people bank account. Every donation helps us in delivering top-notch content. Thank you for being a part of our journey!"
+);
+
+paragraph.appendChild(text);
